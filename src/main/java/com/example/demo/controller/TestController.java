b40dfcc2f80ea162703fb5f65e7baf5eb30088d4
soyoonjeong <76814748+soyoonjeong@users.noreply.github.com>
2023-04-24 18:57:02 +0900

    test controller

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,25 +1,34 @@
 package com.example.demo.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
-@Controller
+@RestController
 @RequestMapping("/login")
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class TestController {
-	@GetMapping("/test")
-	public String getTest(@RequestParam String id, @RequestParam String password) {
-		System.out.println(id);
-		System.out.println(password);
-		return "get 요청" + "id = " +id + "- password ="+ password;
+	@PostMapping("/test")
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
+	public String getTest(@RequestBody Map<String, String> requestData) {
+		requestData.forEach((key, value) -> {
+            System.out.println("key : " + key);
+            System.out.println("value : " + value);
+        });
+		return "yes";
+		//return "{\"response\": \"yes\"}";
 	}
 }
+
