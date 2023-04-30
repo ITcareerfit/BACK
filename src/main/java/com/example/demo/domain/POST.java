ff1cc59247c8091a69fb7210963c2d2f0071de29
고다현 <@LAPTOP-CJEKH138>
2023-04-30 17:43:03 +0900

    ,,,

--- src/main/java/com/example/demo/domain/POST.java
+++ src/main/java/com/example/demo/domain/POST.java
@@ -4,11 +4,13 @@ import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
