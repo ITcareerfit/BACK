dc5f100f82d991c05c48ef07a4fcfa652e305adc
고다현 <@LAPTOP-CJEKH138>
2023-05-12 19:01:45 +0900

    commit

--- src/main/java/com/example/demo/repository/PostRepository.java
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -24,7 +24,7 @@ public interface PostRepository extends JpaRepository<POST, Integer> {
 	POST findByInfoId(Integer infoId);
 
 	@Query(value = "SELECT p.info_id, p.content, p.dead, p.deadline, p.info_pos, p.info_tech, p.last_check, p.d_day, "
-			+ "p.max_career, p.min_career, p.max_pay, p.min_pay, p.title, p.type, p.info_cp_name, p.info_loc, p.info_pos_list, p.info_tech_list "
+			+ "p.max_career, p.min_career, p.max_pay, p.min_pay, p.title, p.type, p.info_cp_name, p.info_loc, p.info_pos_list, p.info_tech_list, p.reg_date "
 			+ "FROM post p, company c "
 			+ "WHERE p.info_cp_name = c.cp_name "
 			+ "AND p.dead = 0 "
@@ -37,7 +37,7 @@ public interface PostRepository extends JpaRepository<POST, Integer> {
 			@Param("employee")int employee, @Param("pay")int pay, @Param("career")int career);
 	
 	@Query(value = "SELECT p.info_id, p.content, p.dead, p.deadline, p.info_pos, p.info_tech, p.last_check, p.d_day, "
-			+ "p.max_career, p.min_career, p.max_pay, p.min_pay, p.title, p.type, p.info_cp_name, p.info_loc, p.info_pos_list, p.info_tech_list "
+			+ "p.max_career, p.min_career, p.max_pay, p.min_pay, p.title, p.type, p.info_cp_name, p.info_loc, p.info_pos_list, p.info_tech_list, p.reg_date "
 			+ "FROM post p "
 			+ "WHERE p.dead = 0 "
 			+ "AND p.info_cp_name = :cpname ", nativeQuery = true)
